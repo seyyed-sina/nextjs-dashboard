e34d3cf5926e86e477b20472ffeb1537a23760a1
sina <cna_fp@yahoo.com>
2025-05-03 15:21:47 +0330

    Fix hidden input name in LoginForm to use 'redirectTo' instead of 'callbackUrl'

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -67,7 +67,7 @@ export default function LoginForm() {
             </div>
           </div>
         </div>
-        <input type="hidden" name="callbackUrl" value={callbackUrl} />
+        <input type="hidden" name="redirectTo" value={callbackUrl} />
         <Button className="mt-4 w-full" aria-disabled={isPending} type="submit">
           Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
         </Button>
